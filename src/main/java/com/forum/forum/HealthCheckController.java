a2283e9e96159cecb63dbbe4bd20ab9ea286c41d
gmailwork <alexanderforenterprise@gmail.com>
2025-09-10 15:54:15 -0400

    First Commit

---
+++ src/main/java/com/forum/forum/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.forum.forum;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ping")
+public class HealthCheckController {
+
+   @GetMapping
+   public String healthCheck() {
+       return "pong";
+   }
+}
