24187153e1540b6a1f22b7bd4bd16ef8ff26e945
gmailwork <alexanderforenterprise@gmail.com>
2025-09-21 15:39:26 -0400

    Update register and login feature

--- src/main/java/com/forum/forum/controller/UserController.java
+++ src/main/java/com/forum/forum/controller/UserController.java
@@ -1,19 +1,41 @@
 package com.forum.forum.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.forum.forum.services.UserService;
+import com.forum.forum.dto.UserLoginRequest;
+import com.forum.forum.dto.UserRegisterRequest;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/register")
-    public String RegisterResponse() {
-        return "User registered successfully.";
+    public ResponseEntity<String> RegisterResponse(@RequestBody UserRegisterRequest request) {
+        boolean result = userService.registerFunc(request);
+        if(result == true){
+            return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully.");
+        }else{
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("User can't register.");
+        }
     }
+
     @PostMapping("/login")
-    public String LoginResponse() {
-        return "User logged in successfully.";
+    public ResponseEntity<String> LoginResponse(@RequestBody UserLoginRequest request) {
+        boolean result = userService.loginFunc(request);
+        if(result == true){
+            return ResponseEntity.status(HttpStatus.OK).body("User logged in successfully.");
+        }else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User can not log in.");
+        }
     }
 }
 
