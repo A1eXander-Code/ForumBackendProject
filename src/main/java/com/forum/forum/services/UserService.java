33c99a40d6e94c3b8dc778f314c6e3ebaafcb41e
gmailwork <alexanderforenterprise@gmail.com>
2025-09-21 15:39:26 -0400

    Update register and login feature

---
+++ src/main/java/com/forum/forum/services/UserService.java
@@ -0,0 +1,42 @@
+package com.forum.forum.services;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.forum.forum.dto.UserLoginRequest;
+import com.forum.forum.dto.UserRegisterRequest;
+import com.forum.forum.model.User;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    
+    private Map<String, User> database = new HashMap<>();
+
+    public boolean registerFunc(UserRegisterRequest request){
+        if(database.containsKey(request.getUsername()) == true){
+            return false;
+        }
+        
+        User newUser = new User();
+        newUser.setUsername(request.getUsername());
+        newUser.setPassword(request.getPassword());
+        database.put(newUser.getUsername(), newUser);
+
+        return true;
+    }
+
+    public boolean loginFunc(UserLoginRequest request){
+        if(database.containsKey(request.getUsername()) == false){
+            return false;
+        }
+
+        User user = database.get(request.getUsername());
+        if(user.getPassword().equals(request.getPassword())){
+            return true;
+        }
+
+        return false;
+    }
+}
